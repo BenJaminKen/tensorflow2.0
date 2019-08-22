0d4681d681b50b7faa20816b2abb7541c0d1db27
13650772322 <804356962@qq.com>
2019-08-22 11:16:46 +0800

    add heart disease

---
+++ HeartDisease/HeartDisease.py
@@ -0,0 +1,14 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import numpy as np
+import pandas as pd
+
+import tensorflow as tf
+
+from tensorflow import feature_column
+from tensorflow.keras import layers
+from sklearn.model_selection import train_test_split
+
+URL = 'https://storage.googleapis.com/applied-dl/heart.csv'
+dataframe = pd.read_csv(URL)
+dataframe.head()
